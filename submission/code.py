1792c192edaa5352a35b538baeba75e48811cd8a
Lorena Arroyo Perez <74391337+Lore1s2know@users.noreply.github.com>
2024-10-03 18:35:35 -0400

    Improved Lab4 code .
    It works better now and I added comments.

--- submission/code.py
+++ submission/code.py
@@ -1,34 +1,40 @@
+
 #The program can be run again until the user decides to quit by pressing the key 'q'. Use a while loop.
-#
-#
+
 import time
 from adafruit_circuitplayground import cp
 
 cp.pixels.auto_write = False
 cp.pixels.brightness = 0.3
 
-color = int(input('Select color. Press 1 for red, 2 for green or 3 for blue: '))
-
 red = 1
 green = 2
 blue = 3
 
+
 max_brightness = 255
-while max_brightness > 0:
-    for i in range(10):
-        if color == 1:
-            cp.pixels[i] = (max_brightness, 0, 0)
-        elif color == 2:
-            cp.pixels[i] = (0, max_brightness, 0)
-        elif color == 3:
-            cp.pixels[i] = (0, 0, max_brightness)
-        else:
-            print("Invalid entry")
-    cp.pixels.show()
-    time.sleep(0.3)
-    max_brightness -= 1
-    
-    quit = input("Would you like to quit? Press q to exit: ").lower()
-    if quit == 'q':
-        print("Program ended")
-        break
+while max_brightness == 255:
+    try: 
+        color = int(input('Select color. Press 1 for red, 2 for green or 3 for blue: '))
+
+        for i in range(10):
+            if color == 1:
+                cp.pixels[i] = (max_brightness, 0, 0)
+            elif color == 2:
+                cp.pixels[i] = (0, max_brightness, 0)
+            elif color == 3:
+                cp.pixels[i] = (0, 0, max_brightness)
+            else:
+                print("Invalid entry")
+        cp.pixels.show()
+        time.sleep(0.0033)
+        
+        quit = input("Would you like to quit? Press q to exit or any other key to continue: ").lower()
+        if quit == 'q':
+            print("Program ended")
+        #Turn off the leds
+            for i in range(10):
+                cp.pixels[i] = (max_brightness,max_brightness,max_brightness)
+            max_brightness -= 1
+    except:
+        print('Invalid input. Try again')
--- submission/lab4.py
+++ submission/lab4.py
@@ -1,34 +1,48 @@
+
 #The program can be run again until the user decides to quit by pressing the key 'q'. Use a while loop.
-#
-#
+
 import time
 from adafruit_circuitplayground import cp
 
 cp.pixels.auto_write = False
 cp.pixels.brightness = 0.3
 
-color = int(input('Select color. Press 1 for red, 2 for green or 3 for blue: '))
-
 red = 1
 green = 2
 blue = 3
 
+#This is the control variable
 max_brightness = 255
-while max_brightness > 0:
-    for i in range(10):
-        if color == 1:
-            cp.pixels[i] = (max_brightness, 0, 0)
-        elif color == 2:
-            cp.pixels[i] = (0, max_brightness, 0)
-        elif color == 3:
-            cp.pixels[i] = (0, 0, max_brightness)
-        else:
-            print("Invalid entry")
-    cp.pixels.show()
-    time.sleep(0.3)
-    max_brightness -= 1
-    
-    quit = input("Would you like to quit? Press q to exit: ").lower()
-    if quit == 'q':
-        print("Program ended")
-        break
+
+#Entering the while loop
+while max_brightness == 255:
+    try: 
+        color = int(input('Select color. Press 1 for red, 2 for green or 3 for blue: '))
+
+        for i in range(10):
+            #This is the if else statements to choose colors
+            if color == 1:
+                cp.pixels[i] = (max_brightness, 0, 0)
+            elif color == 2:
+                cp.pixels[i] = (0, max_brightness, 0)
+            elif color == 3:
+                cp.pixels[i] = (0, 0, max_brightness)
+            else:
+                print("Invalid entry")
+        cp.pixels.show()
+        time.sleep(0.3)
+        
+        quit = input("Would you like to quit? Press q to exit or any other key to continue: ").lower()
+        if quit == 'q':
+            print("Program ended")
+        #Turn off the leds
+            for i in range(10):
+                cp.pixels[i] = (max_brightness,max_brightness,max_brightness)
+                print(f'Turning off Leds {i}')
+        #This will break the loop
+            max_brightness -= 1 
+    except:
+        print('Invalid input. Try again')
+        
+        #Program worked in Thonny using Circuitpy Playground Express
+        
